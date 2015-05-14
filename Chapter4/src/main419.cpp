341540a332fdc969fed3d9d39b871d3eedeb8552
hrghope@gmail.com <hrghope@gmail.com>
2015-05-14 13:08:43 +0800

    refact

--- Chapter4/src/main419.cpp
+++
@@ -1,9 +0,0 @@
-int main419() {
-	int i;
-	//const int ic; //error const variable must be initialized in definition
-	const int *pic;
-	//int *const cpi;  //const point must be initialized in definition
-	//const int *const cpic; // error,both
-
-	return 0;
-}
